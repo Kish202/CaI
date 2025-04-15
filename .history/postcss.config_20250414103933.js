3c3db6ee541c021c59b4d923321e699148b98a88
Kish202 <88828994+Kish202@users.noreply.github.com>
2025-04-15 23:44:56 +0530

    Delete .history directory

--- .history/postcss.config_20250414103933.js
+++
@@ -1,2 +0,0 @@
-<nishikaj@1stzoom.com>
-Cc: Meenakshi Sati <meenakshisati93@gmail.com>
--- .history/src/components/ui/navbar/Navbar_20250413222517.jsx
+++
@@ -1,7 +0,0 @@
-now make a navbar that aligns with the colors of bakground of page:
-componenets in it: left side logo, in center there is curvy rectangle that have 3 items,
-about, home, contact, they scales up plus shows bg shadow when hover on it.
- then at left there is signin/signup button that have sligh border that shows shadow on over, since for now i dont have data so use mock api for it.
-logo must scale up with shadow on hover.
-use neumorphic theme dark bright glowy black to dark bright glow purple gradient colores, shdccn componenets, taiilwindcss v4 in the project.
-then crate vite,config file for the custom theme u used in the project
